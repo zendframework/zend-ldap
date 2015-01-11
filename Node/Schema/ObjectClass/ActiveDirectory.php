8251dae9170f14729867c60cfbb257f4c35bd56d
Marco Pivetta <ocramius@gmail.com>
2015-01-11 17:48:29 +0000

    Importing state as of zendframework/zf2@d99f5c90927e71cb3cbfeeca10d27d79942c21c0 (1420998509)
    Automatic import via rsync

--- Node/Schema/ObjectClass/ActiveDirectory.php
+++ Node/Schema/ObjectClass/ActiveDirectory.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -34,6 +34,7 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getOid()
     {
+
     }
 
     /**
@@ -43,6 +44,7 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getMustContain()
     {
+
     }
 
     /**
@@ -52,6 +54,7 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getMayContain()
     {
+
     }
 
     /**
@@ -61,6 +64,7 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getDescription()
     {
+
     }
 
     /**
@@ -70,6 +74,7 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getType()
     {
+
     }
 
     /**
@@ -80,5 +85,6 @@ class ActiveDirectory extends Schema\AbstractItem implements ObjectClassInterfac
      */
     public function getParentClasses()
     {
+
     }
 }
