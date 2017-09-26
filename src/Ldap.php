c5bc85dc50f88f40c6329dfb696c2718f968df57
Mike Baynton <mike@mbaynton.com>
2017-09-26 13:47:29 -0500

    CS

--- src/Ldap.php
+++ src/Ldap.php
@@ -980,7 +980,7 @@ class Ldap
             }
 
             if ($this->shouldReconnect($this->resource)) {
-              return $this;
+                return $this;
             }
 
             $message = ($username === null) ? $this->connectString : $username;
